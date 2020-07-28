ae1e162f83eb77740b6996e1eaad42ef2e13f066
Epicrider <omarepic1@gmail.com>
2020-07-28 16:50:19 -0700

    Change Img src for about me page

--- js/index.js
+++ js/index.js
@@ -3,9 +3,9 @@ var aboutMeButtonProgramming = document.getElementById("ProgrammingButton");
 var aboutMeButtonDesign = document.getElementById("DesignButton");
 
 var aboutMeDescription = document.getElementById("aboutMeDescriptionText");
-var aboutMeImage = document.getElementById("");
+var aboutMeImage = document.getElementById("aboutMeImage");
 
-var switchCraft = function(newDescription) { aboutMeDescription.innerHTML = newDescription;};
+var switchCraft = function(newDescription, imageSource) { aboutMeDescription.innerHTML = newDescription; aboutMeImage.src = imageSource};
 
 aboutMeButtonArtist.addEventListener("click", function () {
   switchCraft("<br>I use a variety of mediums to convey a message, whether it be through video games, sketches, and paintings.<br>"
@@ -18,7 +18,8 @@ aboutMeButtonArtist.addEventListener("click", function () {
               + "- Painting with watercolors<br><br>"
               + "As every artist, I am always practicing and learning to perfect my craft as much as possible. Gesture Drawing and drawig references<br>"
               + "is daily practice to me. At the moment, I am practicing my skills in digital art to be able to be used in a professional level.<br>"
-              + "To learn more about what projects and practical work I have done, check the My Work section below!");
+              + "To learn more about what projects and practical work I have done, check the My Work section below!",
+              "img/Art1.gif");
 });
 aboutMeButtonProgramming.addEventListener("click", function () {
   switchCraft("<br>I am currently a Computer Science Student at the University of California at Irvine, planning to graduate by my third year in the class of 2022.<br>"
@@ -30,7 +31,8 @@ aboutMeButtonProgramming.addEventListener("click", function () {
                + " - The ability to use Version Control such as Git <br> - Ability to plan out code that must be done in a team"
                + "<br><br> I am always looking for new things to learn. At the moment I am dabbling in Machine Learning and learning how to utilize graphic interfaces<br>"
                + "such as OpenGL."
-               + "<br>To learn more about what projects and practical work I have done, check the My Work section below!");
+               + "<br>To learn more about what projects and practical work I have done, check the My Work section below!",
+               "img/Coding1.gif");
 });
 aboutMeButtonDesign.addEventListener("click", function () {
   switchCraft("<br>Coming Soon!");
