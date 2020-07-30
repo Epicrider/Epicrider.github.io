85c25079837e1293d49c5c404b89e46181e520d4
Epicrider <omarepic1@gmail.com>
2020-07-30 09:29:32 -0700

    Embed imgur link rather than load who gif on site

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ aboutMeButtonArtist.addEventListener("click", function () {
               + "As every artist, I am always practicing and learning to perfect my craft as much as possible. Gesture Drawing and drawig references<br>"
               + "is daily practice to me. At the moment, I am practicing my skills in digital art to be able to be used in a professional level.<br>"
               + "To learn more about what projects and practical work I have done, check the My Work section below!",
-              "img/Art1.gif");
+              "https://i.imgur.com/oNJ7Cmd.gif");
 });
 aboutMeButtonProgramming.addEventListener("click", function () {
   switchCraft("<br>I am currently a Computer Science Student at the University of California at Irvine, planning to graduate by my third year in the class of 2022.<br>"
@@ -32,7 +32,7 @@ aboutMeButtonProgramming.addEventListener("click", function () {
                + "<br><br> I am always looking for new things to learn. At the moment I am dabbling in Machine Learning and learning how to utilize graphic interfaces<br>"
                + "such as OpenGL."
                + "<br>To learn more about what projects and practical work I have done, check the My Work section below!",
-               "img/Coding1.gif");
+               "https://i.imgur.com/QlAaHC8.gif");
 });
 aboutMeButtonDesign.addEventListener("click", function () {
   switchCraft("<br>Coming Soon!");
