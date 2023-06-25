4a35583bb28992ae083b213ee75ffa7390ac4d47
Epicrider <ohossain@uci.edu>
2023-06-25 14:20:45 -0700

    Format Me Picture and Add Simple Navigation

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,9 +4,9 @@ import {BsLinkedin, BsInstagram, BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header_socials'>
-        <a href='https://www.linkedin.com/in/omar-hossain-engineering/' target='_blank'><BsLinkedin/></a>
-        <a href='https://github.com/Epicrider' target='_blank'><BsGithub/></a>
-        <a href='https://www.instagram.com/omarh_rider/' target='_blank'><BsInstagram/></a>
+        <a href='https://www.linkedin.com/in/omar-hossain-engineering/' target='_blank' rel="noreferrer"><BsLinkedin/></a>
+        <a href='https://github.com/Epicrider' target='_blank' rel="noreferrer"><BsGithub/></a>
+        <a href='https://www.instagram.com/omarh_rider/' target='_blank' rel="noreferrer"><BsInstagram/></a>
     </div>
   )
 }
