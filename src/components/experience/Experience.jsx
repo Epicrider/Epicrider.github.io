7d465de303aee6e78d7700338abba6540f2e80f6
Epicrider <ohossain@uci.edu>
2023-06-28 14:34:05 -0700

    Add Experience Cards and Fixed Formatting

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -7,34 +7,45 @@ const Experience = () => {
     <section id='experience'>
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
-      <div className="container experience__robotics">
-        <div className="experience_robotics">
+
+      <div className="container experience__container">
+        <div className="experience__robotics">
           <h3>Robotics Development</h3>
           <div className="experience__content">
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
               <h4>ROS</h4>
               <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
               <h4>JavaScript</h4>
               <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
               <h4>Gazebo</h4>
               <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
               <h4>Python</h4>
               <small className='text-light'>Experienced</small>
+              </div>
             </article>
             <article className='experience__details'>
-              <BsPatchCheckFill />
+              <BsPatchCheckFill className='experience__details-icon' />
+              <div>
               <h4>Unity</h4>
               <small className='text-light'>Experienced</small>
+              </div>
             </article>
           </div>
         </div>
@@ -45,34 +56,43 @@ const Experience = () => {
       <h3>Game Development</h3>
         <div className="experience__content">
           <article className='experience__details'>
-            <BsPatchCheckFill />
+            <BsPatchCheckFill className='experience__details-icon' />
+            <div>
             <h4>Unity</h4>
             <small className='text-light'>Experienced</small>
+            </div>
           </article>
           <article className='experience__details'>
-            <BsPatchCheckFill />
+            <BsPatchCheckFill className='experience__details-icon' />
+            <div>
             <h4>Godot</h4>
             <small className='text-light'>Experienced</small>
+            </div>
           </article>
           <article className='experience__details'>
-            <BsPatchCheckFill />
+            <BsPatchCheckFill className='experience__details-icon' />
+            <div>
             <h4>Unreal Engine</h4>
             <small className='text-light'>Experienced</small>
+            </div>
           </article>
           <article className='experience__details'>
-            <BsPatchCheckFill />
+            <BsPatchCheckFill className='experience__details-icon' />
+            <div>
             <h4>C#</h4>
             <small className='text-light'>Experienced</small>
+            </div>
           </article>
           <article className='experience__details'>
-            <BsPatchCheckFill />
+            <BsPatchCheckFill className='experience__details-icon' />
+            <div>
             <h4>C++</h4>
             <small className='text-light'>Experienced</small>
+            </div>
           </article>
         </div>
       </div>
       </div>
-      <div className="experience__backend"></div>
     </section>
   )
 }
