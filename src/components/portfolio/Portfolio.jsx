ffd8479ed2ebe1769ed17e828b4d26b8c6ff0cbc
Epicrider <ohossain@uci.edu>
2023-06-29 17:11:45 -0700

    Add Simple Portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './portfolio.css'
+// import IMG1 from '../../assets/'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+      
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image"></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/Epicrider" className='btn'>Github</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank' rel="noreferrer">Live Demo</a>
+
+        </article>
+      </div>
+    </section>
   )
 }
 
