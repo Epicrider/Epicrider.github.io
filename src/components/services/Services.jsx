822efe9a307acfd8f22b27a968169e917f1d6d00
Epicrider <ohossain@uci.edu>
2023-08-01 11:58:04 -0700

    Fix Services to Specialties and Add information

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -5,37 +5,10 @@ import {BiCheck} from 'react-icons/bi'
 const Services = () => {
   return (
     <section id='services'>
-      <h5>What I Offer</h5>
-      <h2>Services</h2>
+      <h5>What I offer</h5>
+      <h2>Specialties</h2>
 
       <div className="container services__container">
-        <artcile className="service">
-          <div className='service__head'>
-            <h3>Robotics</h3>
-          </div>
-          <ul className='service__list'>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-            </li>
-          </ul>
-        </artcile>
         {/* End of UI/UX Design */}
         <artcile className="service">
           <div className='service__head'>
@@ -44,27 +17,50 @@ const Services = () => {
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+              <p>Ability to program any functionality to the game, from game mechanics to UI</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+              <p>Design varying environments that make use of game mechanics in new and clever ways</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+              <p>Ability to lead programmers, artists, and musicians to one clear vision in a directing role</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
-            </li>
-            <li>
-              <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+              <p>Advanced knowledge of the Unity Engine and transferrable skills to other popular engines (such as Unreal Engine)</p>
             </li>
           </ul>
         </artcile>
         {/* End of stuff */}
+        <artcile className="service">
+          <div className='service__head'>
+            <h3>Robotics</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Interfacing with robots, including multi-agent scenarios utilzing ROS</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Vehicle Motion Planning, with knowledge of Control Theory and Path Planning algorithms</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Simulation through tools such as Gazebo and Issacsym</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Versed in contemporary knowledge of Reinforcement Learning and Training for various robotics</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon' />
+              <p>Able to aid in research, development, and deployment in any stage of robotics development</p>
+            </li>
+          </ul>
+        </artcile>
         <artcile className="service">
           <div className='service__head'>
             <h3>Web Development</h3>
@@ -72,23 +68,23 @@ const Services = () => {
           <ul className='service__list'>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+              <p>Provide Backend support with knowledge of creating APIs and utilizing connected services</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+              <p>Ability to add upon large code bases spanning years of work by a high-tech company</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+              <p>Well versed in Amazon Web Services (AWS)</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+              <p>Ability to design solutions for customer's needs around the AWS ecosystem</p>
             </li>
             <li>
               <BiCheck className='service__list-icon' />
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+              <p>Proficient experience in Front End including HTML, CSS, Javscript / Typescript, and React Native</p>
             </li>
           </ul>
         </artcile>
